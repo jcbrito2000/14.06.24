14b5390c830ccd6da57b5fa1fce3ccd1a079d568
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-13 22:30:29 -0300

    Add files via upload

---
+++ script_inserir_registro.sql
@@ -0,0 +1,8 @@
+INSERT INTO `db_alunos`.`tbl_alunos` 
+(`nome_aluno`, `nome_do_pai`, `nome_mae`, `whatsapp_pais`) 
+VALUES ('marcos', 'mario', 'renata', '4399693636');
+INSERT INTO `db_alunos`.`tbl_alunos` 
+(`nome_aluno`, `nome_do_pai`, `nome_mae`, `whatsapp_pais`)
+ VALUES ('fabiana', 'pedro', 'paula', '4496396969');
+INSERT INTO `db_alunos´`.`tbl_alunos` (`nome_aluno`, `nome_do_pai`, `nome_mae`, `whatsapp_pais`)
+ VALUES ('hugo', 'jean', 'solange', '1198484252');
